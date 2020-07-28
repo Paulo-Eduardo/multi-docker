fc3205a4180eae0d5a219a49309584e3c2b74b13
Paulo Eduardo <pauloes.dev@gmail.com>
2020-07-28 19:08:01 -0300

    removing header

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
-        </header>
+        </header> */}
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
